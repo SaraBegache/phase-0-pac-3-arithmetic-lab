7013ffa9f3c96fd9ca671a3a2af3af810b6feda1
Sara BEGACHE <sara.begache@gmail.com>
2023-07-22 23:28:55 +0100

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+// Basic math functions
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Helper function to parse a string into an integer
+  function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+  // Helper function to parse a string into a floating-point number
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
